0b1fdcdfb83e8b1a1ee818c060d194f902b2bdf1
pablo Coronel <pablomanuelcoronel@gmail.com>
2019-04-25 19:17:55 -0300

    usando un iterador para recorrer y eliminar elementos

--- src/Cuentas_Usuarios.java
+++ src/Cuentas_Usuarios.java
@@ -23,16 +23,47 @@ public class Cuentas_Usuarios {
 		clientes_banco.add(cliente_3);
 		clientes_banco.add(cliente_4);
 
-		// agrego el objecto reperito al hashSet (en teoria no admite repetidos)
+		// agrego el objeto repetido al hashSet (en teoria no admite repetidos)
 		// aparece repetido porque para clases propias (no predefinidas), es necesrio
 		// sobreescribir el metodo hashCode() y equals()
 		clientes_banco.add(cliente_5);
 
-		// recorrer la coleccion
-		for (Cliente un_cliente : clientes_banco) {
-			System.out.println(
-					un_cliente.getNombre() + " " + un_cliente.getNumero_cuenta() + " " + un_cliente.getSaldo());
+		// recorrer la coleccion con un foreach
+//		for (Cliente un_cliente : clientes_banco) {
+//			System.out.println(
+//					un_cliente.getNombre() + " " + un_cliente.getNumero_cuenta() + " " + un_cliente.getSaldo());
+//		}
+
+		/****************************************/
+		// Eliminar elemento de una coleccion
+		// en un foreach nomral lanza una excepcion, ya que no se puede modificar la
+		// coleccion mientras se recorre
+		// con un iterador es posible
+
+		// el iterador contiene el objeto devuelto por el metodo iterator() de la
+		// coleccion
+		Iterator<Cliente> it_borrar = clientes_banco.iterator();
+		while (it_borrar.hasNext()) {
+			String nombre = it_borrar.next().getNombre();
+
+			if (nombre.equals("cacho")) {
+				// lo quita de la coleccion
+				it_borrar.remove();
+			}
 		}
+
+		// recorrer la coleccion con un iterador
+		Iterator<Cliente> it = clientes_banco.iterator();
+
+		while (it.hasNext()) {
+//			String nombre_cliente = it.next().getNombre();
+//			System.out.println(nombre_cliente);
+
+			Cliente un_cliente = it.next();
+			System.out.println(un_cliente.getNombre());
+			System.out.println(un_cliente.getNumero_cuenta());
+		}
+
 	}
 
 }

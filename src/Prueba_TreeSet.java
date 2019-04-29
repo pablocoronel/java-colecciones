7dc774c8b0069308a5696d75bf74cfe7ab820ad5
pablo Coronel <pablomanuelcoronel@gmail.com>
2019-04-28 23:03:54 -0300

    treeset con Comparator (método compare)

--- src/Prueba_TreeSet.java
+++ src/Prueba_TreeSet.java
@@ -1,3 +1,4 @@
+import java.util.Comparator;
 import java.util.TreeSet;
 
 public class Prueba_TreeSet {
@@ -21,24 +22,47 @@ public class Prueba_TreeSet {
 		Articulo segundo = new Articulo(2, "Segundo articulo");
 		Articulo tercero = new Articulo(3, "Tercer articulo");
 		Articulo cuarto = new Articulo(4, "Cuarto articulo");
-		
+
 		TreeSet<Articulo> ordena_articulos = new TreeSet<Articulo>();
 		ordena_articulos.add(primero);
 		ordena_articulos.add(tercero);
 		ordena_articulos.add(cuarto);
 		ordena_articulos.add(segundo);
 
-		
 		for (Articulo articulo : ordena_articulos) {
 			System.out.println(articulo.getDescripcion());
+		}
+
+		/*********************************************************/
+		// con Comparator
+		// version de Articulo con comparator
+		Articulo comparador_articulos = new Articulo();
+
+		// TreeSet que recibe un Comparator
+		// el TreeSet busca directamente el metodo compare()
+		TreeSet<Articulo> ordena_articulos_2 = new TreeSet<Articulo>(comparador_articulos);
+
+		ordena_articulos_2.add(primero);
+		ordena_articulos_2.add(segundo);
+		ordena_articulos_2.add(tercero);
+		ordena_articulos_2.add(cuarto);
+
+		// ver
+		System.out.println();
+		for (Articulo art : ordena_articulos_2) {
+			System.out.println(art.getDescripcion());
 		}
 	}
 
 }
 
-class Articulo implements Comparable<Articulo> {
+class Articulo implements Comparable<Articulo>, Comparator<Articulo> {
 	private int numero;
 	private String descripcion;
+
+	public Articulo() {
+
+	}
 
 	public Articulo(int numero, String descripcion) {
 		// TODO Auto-generated constructor stub
@@ -62,5 +86,15 @@ class Articulo implements Comparable<Articulo> {
 
 	public String getDescripcion() {
 		return this.descripcion;
+	}
+
+	@Override
+	public int compare(Articulo arg0, Articulo arg1) {
+		// TODO Auto-generated method stub
+
+		String descripcionA = arg0.getDescripcion();
+		String descripcionB = arg1.getDescripcion();
+
+		return descripcionA.compareTo(descripcionB);
 	}
 }
